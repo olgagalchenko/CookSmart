3e4f2bdd4f2a46b73a6d08ef9f655e95266ceb5c
Olga Galchenko <olga@galchenko.com>
2013-10-08 22:44:59 -0700

    empty project that is soon to be the greatest app in the world

---
+++ CookSmart/CookSmart/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  CookSmart
+//
+//  Created by Olga Galchenko on 10/8/13.
+//  Copyright (c) 2013 Olga Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
