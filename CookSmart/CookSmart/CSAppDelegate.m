f0be65f1601007388e0cf4eb126446e6ff1f0370
Olga Galchenko <olga@galchenko.com>
2014-02-04 14:52:30 -0800

    Adding TestFlight

--- CookSmart/CookSmart/CSAppDelegate.m
+++ CookSmart/CookSmart/CSAppDelegate.m
@@ -9,11 +9,14 @@
 #import "CSAppDelegate.h"
 #import "CSConversionVC.h"
 #import "CSIngredients.h"
+#import "TestFlight.h"
 
 @implementation CSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [TestFlight takeOff:@"33e4b92b-0234-47cb-b533-e676909b43da"];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
