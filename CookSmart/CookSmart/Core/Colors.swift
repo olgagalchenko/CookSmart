07116b13644ea66a9a85f28b3ef90404a8e56089
Alex King <alexking124@gmail.com>
2020-03-28 12:04:51 -0700

    Update formatting

--- CookSmart/CookSmart/Core/Colors.swift
+++ CookSmart/CookSmart/Core/Colors.swift
@@ -10,11 +10,15 @@ import UIKit
 
 enum Color {
 
-    static let redLineColor = Color.color(light: UIColor(red: 187.0/255.0, green: 1.0/255.0, blue: 3.0/255.0, alpha: 1.0),
-                                          dark: UIColor(red: 187.0/255.0, green: 1.0/255.0, blue: 3.0/255.0, alpha: 1.0))
+    static let redLineColor = Color.color(
+        light: UIColor(red: 187.0/255.0, green: 1.0/255.0, blue: 3.0/255.0, alpha: 1.0),
+        dark: UIColor(red: 187.0/255.0, green: 1.0/255.0, blue: 3.0/255.0, alpha: 1.0)
+    )
 
-    static let background = Color.color(light: UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0),
-                                        dark: UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0))
+    static let background = Color.color(
+        light: UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0),
+        dark: UIColor(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
+    )
 
     private static func color(light: UIColor, dark: UIColor) -> UIColor {
         return UIColor { traitCollection in
