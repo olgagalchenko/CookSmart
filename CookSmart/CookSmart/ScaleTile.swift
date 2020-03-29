ac9748c2324ce81f816b165cbf73fa46b2275ac8
Alex King <alexking124@gmail.com>
2020-03-28 17:59:22 -0700

    Fixed Mint Scripts (#14)
    * Fixed Mint Scripts
    
    * Fixing Swiftformat config path

--- CookSmart/CookSmart/ScaleTile.swift
+++ CookSmart/CookSmart/ScaleTile.swift
@@ -11,44 +11,44 @@ import SwiftUI
 import UIKit
 
 class ScaleTile: UIView {
-    // MARK: Lifecycle
+  // MARK: Lifecycle
 
-    init(frame: CGRect, mirror: Bool) {
-        self.mirror = mirror
-        super.init(frame: frame)
+  init(frame: CGRect, mirror: Bool) {
+    self.mirror = mirror
+    super.init(frame: frame)
+  }
+
+  required init?(coder _: NSCoder) {
+    fatalError()
+  }
+
+  var value: Double {
+    get { Double(valueLabel.text ?? "") ?? 0 }
+    set {
+      valueLabel.text = String(format: "%1.0f", newValue)
     }
+  }
 
-    required init?(coder _: NSCoder) {
-        fatalError()
-    }
+  // MARK: Private
 
-    var value: Double {
-        get { Double(valueLabel.text ?? "") ?? 0 }
-        set {
-            valueLabel.text = String(format: "%1.0f", newValue)
-        }
-    }
+  let valueLabel: UILabel = UILabel()
+  let mirror: Bool
 
-    // MARK: Private
-
-    let valueLabel: UILabel = UILabel()
-    let mirror: Bool
-
-    override func draw(_: CGRect) {}
+  override func draw(_: CGRect) {}
 }
 
 struct ScaleTilePreview: PreviewProvider {
-    static var previews: some View {
-        TilePreviewContainer()
+  static var previews: some View {
+    TilePreviewContainer()
+  }
+
+  struct TilePreviewContainer: UIViewRepresentable {
+    func makeUIView(context _: UIViewRepresentableContext<ScaleTilePreview.TilePreviewContainer>) -> UIView {
+      ScaleTile(frame: CGRect.zero, mirror: true)
     }
 
-    struct TilePreviewContainer: UIViewRepresentable {
-        func makeUIView(context _: UIViewRepresentableContext<ScaleTilePreview.TilePreviewContainer>) -> UIView {
-            ScaleTile(frame: CGRect.zero, mirror: true)
-        }
+    func updateUIView(_: UIView, context _: UIViewRepresentableContext<ScaleTilePreview.TilePreviewContainer>) {}
 
-        func updateUIView(_: UIView, context _: UIViewRepresentableContext<ScaleTilePreview.TilePreviewContainer>) {}
-
-        typealias UIViewType = UIView
-    }
+    typealias UIViewType = UIView
+  }
 }
