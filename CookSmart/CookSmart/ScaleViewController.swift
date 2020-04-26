837685f2a25ca781a87434e2d4f1afc50e419f59
Alex King <alexking124@gmail.com>
2020-04-26 10:26:57 -0700

    Add Unit Picker to ScaleViewController (#38)
    * Added ability to change units
    
    * Minor cleanup/fixes
    
    * Some cleanup
    
    * Using an enum for display states

--- CookSmart/CookSmart/ScaleViewController.swift
+++ CookSmart/CookSmart/ScaleViewController.swift
@@ -11,6 +11,12 @@ import SwiftUI
 import UIKit
 
 class ScaleViewController: UIViewController {
+
+  private enum DisplayMode {
+    case scales
+    case unitPicker
+  }
+
   init(ingredient: CSIngredient, shouldSyncScales: Bool = true) {
     self.ingredient = ingredient
     super.init(nibName: nil, bundle: nil)
@@ -31,25 +37,44 @@ class ScaleViewController: UIViewController {
   // MARK: Public
 
   var ingredient: CSIngredient
-  var currentVolumeUnit: CSUnit { CSUnitCollection.volumeUnits()?.unit(at: 2) ?? CSUnit() }
-  var currentWeightUnit: CSUnit { CSUnitCollection.weightUnits()?.unit(at: 2) ?? CSUnit() }
-
-  var densityDidChange: ((_ density: Double) -> Void)?
-  var didBeginScrolling: (() -> Void)?
+  var currentVolumeUnit: CSUnit = CSUnitCollection.volumeUnits()?.unit(at: 2) ?? CSUnit()
+  var currentWeightUnit: CSUnit = CSUnitCollection.weightUnits()?.unit(at: 2) ?? CSUnit()
 
   // MARK: Private
 
-  private let volumeUnitButton = Button()
-  private let weightUnitButton = Button()
-  private lazy var scalesContainer = ScalesView(ingredient: ingredient,
-                                                unitConversionFactor: unitConversionFactor,
+  private var displayMode: DisplayMode = .scales
+
+  private let volumeUnitButton: Button = {
+    let button = Button()
+    button.setTitleColor(.label, for: .disabled)
+    button.setTitle("Volume", for: .disabled)
+    button.addTarget(self, action: #selector(toggleDisplayMode), for: .touchUpInside)
+    return button
+  }()
+
+  private let weightUnitButton: Button = {
+    let button = Button()
+    button.setTitleColor(.label, for: .disabled)
+    button.setTitle("Weight", for: .disabled)
+    button.addTarget(self, action: #selector(toggleDisplayMode), for: .touchUpInside)
+    return button
+  }()
+
+  private lazy var scalesContainer = ScalesView(unitConversionFactor: unitConversionFactor,
                                                 syncScales: true)
+  private lazy var unitPickerView = UnitPickerView(volumeUnit: currentVolumeUnit, weightUnit: currentWeightUnit)
+
+  private var scalesTopConstraint: NSLayoutConstraint?
+  private var unitPickerBottomConstraint: NSLayoutConstraint?
 
   private var unitConversionFactor: CGFloat {
     CGFloat(ingredient.density(withVolumeUnit: currentVolumeUnit, andWeightUnit: currentWeightUnit))
   }
 
   private func setUpViews() {
+    view.clipsToBounds = true
+    unitPickerView.delegate = self
+
     setUpScaleViews()
     setUpUnitViews()
   }
@@ -57,27 +82,60 @@ class ScaleViewController: UIViewController {
   private func setUpUnitViews() {
     let gradientView = GradientView()
     view.addSubview(gradientView)
-    gradientView.constrainToSuperview(anchors: [.leading, .top, .right], priority: .defaultHigh, shouldActivate: true)
+    gradientView.constrainToSuperview(anchors: [.leading, .top, .right])
     gradientView.heightAnchor.constraint(equalToConstant: 100).isActive = true
 
     gradientView.addSubview(volumeUnitButton)
-    volumeUnitButton.constrainToSuperview(anchors: [.leading, .top], priority: .defaultHigh, shouldActivate: true)
+    volumeUnitButton.constrainToSuperview(anchors: [.leading, .top])
 
     gradientView.addSubview(weightUnitButton)
-    weightUnitButton.constrainToSuperview(anchors: [.trailing, .top], priority: .defaultHigh, shouldActivate: true)
+    weightUnitButton.constrainToSuperview(anchors: [.trailing, .top])
 
-    volumeUnitButton.trailingAnchor.constraint(equalTo: weightUnitButton.leadingAnchor, constant: 0).isActive = true
-    volumeUnitButton.widthAnchor.constraint(equalTo: weightUnitButton.widthAnchor, multiplier: 1).isActive = true
+    volumeUnitButton.trailingAnchor.constraint(equalTo: weightUnitButton.leadingAnchor).isActive = true
+    volumeUnitButton.widthAnchor.constraint(equalTo: weightUnitButton.widthAnchor).isActive = true
   }
 
   private func setUpScaleViews() {
-    scalesContainer.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(scalesContainer)
-    scalesContainer.constrainToSuperview()
+    scalesContainer.constrainToSuperview(anchors: [.leading, .trailing, .height])
+    scalesTopConstraint = scalesContainer.topAnchor.constraint(equalTo: view.topAnchor)
+    scalesTopConstraint?.isActive = true
+
+    view.addSubview(unitPickerView)
+    unitPickerView.constrainToSuperview(anchors: [.leading, .trailing, .height])
+    unitPickerBottomConstraint = unitPickerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+
+    unitPickerView.topAnchor.constraint(equalTo: scalesContainer.bottomAnchor).isActive = true
   }
 
   private func setContent() {
     volumeUnitButton.setTitle(currentVolumeUnit.name, for: .normal)
     weightUnitButton.setTitle(currentWeightUnit.name, for: .normal)
+  }
+
+  @objc
+  private func toggleDisplayMode() {
+    guard let scalesTopConstraint = scalesTopConstraint,
+      let unitPickerBottomConstraint = unitPickerBottomConstraint else {
+      return
+    }
+    displayMode = (displayMode == .scales) ? .unitPicker : .scales
+    UIViewPropertyAnimator(duration: 0.3, curve: .easeInOut) {
+      scalesTopConstraint.isActive = self.displayMode == .scales
+      unitPickerBottomConstraint.isActive = self.displayMode == .unitPicker
+      self.view.layoutIfNeeded()
+    }.startAnimation()
+    weightUnitButton.isEnabled = displayMode == .scales
+    volumeUnitButton.isEnabled = displayMode == .scales
+  }
+}
+
+extension ScaleViewController: UnitPickerDelegate {
+  func picked(volumeUnit: CSUnit, weightUnit: CSUnit) {
+    currentVolumeUnit = volumeUnit
+    currentWeightUnit = weightUnit
+    setContent()
+    scalesContainer.updateConversionFactor(unitConversionFactor)
+    toggleDisplayMode()
   }
 }
